71acc3af61ff4b915db6f30a4e48a48b619bdb75
amlane <twelvexstring@gmail.com>
2019-06-17 15:28:45 -0500

    created basic tests for succeed, repair, and fail

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,28 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    ++item.enhancement;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) item.durability -= 5;
+  else if (item.enhancement >= 15) item.durability -= 10;
+  else if (item.enhancement >= 16) item.enhancement -= 1;
+  else if (item.enhancement === 0) item.durability;
+  else if (item.durability === 0) item.durability;
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  item = { ...item, durability: 100 };
+  return item;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,35 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("should restore durability to 100", () => {
+      const item = { durability: 0 };
+      expect(enhancer.repair(item).durability).toBe(100);
+    });
+  });
+
+  describe("succeed()", () => {
+    it("should increases enhancement levels by 1", () => {
+      const item = { enhancement: 0 };
+      expect(enhancer.succeed(item).enhancement).toBe(1);
+    });
+
+    it("should not increase enhancement levels by 1 if greater than 20", () => {
+      const item = { enhancement: 20 };
+      expect(enhancer.succeed(item).enhancement).toBe(20);
+    });
+
+    it("should not change durability", () => {
+      const item = { durability: 80 };
+      expect(enhancer.succeed(item).durability).toBe(80);
+    });
+  });
+
+  describe("fail()", () => {
+    it("should subtract 5 from durablity when enhancement is less than 15", () => {
+      const item = { enhancement: 10, durability: 100 };
+      expect(enhancer.fail(item).durability).toBe(95);
+    });
+  });
+});
